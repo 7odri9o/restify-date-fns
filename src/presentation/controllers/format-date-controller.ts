37a293a4e71bb5300a2dea6579e2cfe5f2447df9
Rodrigo Rocha <act111@einstein.br>
2022-03-19 03:13:54 -0300

    refactor: simplify code

--- src/presentation/controllers/format-date-controller.ts
+++ src/presentation/controllers/format-date-controller.ts
@@ -1,16 +1,12 @@
 export class FormatDateController {
   async handle (httpRequest: any): Promise<any> {
-    if (!httpRequest.body.format) {
-      return {
-        status: 400,
-        error: new Error('Missing param: format')
-      }
-    }
-
-    if (!httpRequest.body.date) {
-      return {
-        status: 400,
-        error: new Error('Missing param: date')
+    const requiredFields = ['date', 'format']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          status: 400,
+          error: new Error(`Missing param: ${field}`)
+        }
       }
     }
   }
