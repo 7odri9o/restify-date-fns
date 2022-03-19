62e4f3f35bd727b7f66826959e1b99b1f67317dd
Rodrigo Rocha <act111@einstein.br>
2022-03-19 06:11:12 -0300

    test: ensure FormatDate calls DateFormatter with correct values

---
+++ tests/data/usecases/format-date.test.ts
@@ -0,0 +1,32 @@
+import { FormatDateImpl } from '@/data/usecases/format-date'
+
+import { DateFormatterSpy } from '@/tests/presentation/mocks'
+
+type SutTypes = {
+  sut: FormatDateImpl
+  dateFormatterSpy: DateFormatterSpy
+}
+
+const makeSut = (): SutTypes => {
+  const dateFormatterSpy = new DateFormatterSpy()
+  const sut = new FormatDateImpl(dateFormatterSpy)
+  return {
+    sut,
+    dateFormatterSpy
+  }
+}
+
+describe('FormatDate Usecase', () => {
+  describe('Dependencies Calls', () => {
+    test('Should call DateFormatter with correct values', () => {
+      const { sut, dateFormatterSpy } = makeSut()
+
+      const date = '2022-02-01'
+      const expectedFormat = 'DD/MM/YYYY'
+      sut.format(date, expectedFormat)
+
+      expect(dateFormatterSpy.date).toBe(date)
+      expect(dateFormatterSpy.expectedFormat).toBe(expectedFormat)
+    })
+  })
+})
--- tests/presentation/mocks/mock-format-date.ts
+++ tests/presentation/mocks/mock-format-date.ts
@@ -1,8 +1,22 @@
 import { FormatDate } from '@/domain/usecases/format-date'
+import { DateFormatter } from '@/data/protocols/date-formatter'
 
 import faker from '@faker-js/faker'
 
 export class FormatDateSpy implements FormatDate {
+  date: string
+  expectedFormat: string
+  formattedDate: string
+
+  format (date: string, expectedFormat: string): string {
+    this.date = date
+    this.expectedFormat = expectedFormat
+    this.formattedDate = faker.random.word()
+    return this.formattedDate
+  }
+}
+
+export class DateFormatterSpy implements DateFormatter {
   date: string
   expectedFormat: string
   formattedDate: string
