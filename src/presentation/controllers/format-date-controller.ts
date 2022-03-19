aeb669ced195a842452b3a8cd55aa0518ab25e07
Rodrigo Rocha <act111@einstein.br>
2022-03-19 03:38:00 -0300

    feat: ensure FormatDateController calls Validation with correct values

--- src/presentation/controllers/format-date-controller.ts
+++ src/presentation/controllers/format-date-controller.ts
@@ -1,7 +1,10 @@
-import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class FormatDateController implements Controller {
+  constructor (private readonly validation: Validation) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validation.validate(httpRequest.body)
     const requiredFields = ['date', 'format']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
