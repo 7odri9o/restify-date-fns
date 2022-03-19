0160f94cdb3407a61a65267234de2fa13819747b
Rodrigo Rocha <act111@einstein.br>
2022-03-19 03:25:29 -0300

    refactor: ensure FormatDateController implement correct interfaces

--- src/presentation/controllers/format-date-controller.ts
+++ src/presentation/controllers/format-date-controller.ts
@@ -1,6 +1,6 @@
-import { HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
-export class FormatDateController {
+export class FormatDateController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['date', 'format']
     for (const field of requiredFields) {
