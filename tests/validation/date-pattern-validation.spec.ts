72ef20f142b687168a17e2c358b70cf3f8727860
Rodrigo Rocha <act111@einstein.br>
2022-03-19 15:42:39 -0300

    test: ensure DatePatternValidation returns an InvalidParamError if validation fails

---
+++ tests/validation/date-pattern-validation.spec.ts
@@ -0,0 +1,18 @@
+import { DatePatternValidation } from '@/validation/validators/date-pattern-validation'
+import { InvalidParamError } from '@/presentation/errors'
+
+import faker from '@faker-js/faker'
+
+const field = faker.random.word()
+
+const makeSut = (): DatePatternValidation => {
+  return new DatePatternValidation(field)
+}
+
+describe('DatePattern Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ invalidField: faker.random.word() })
+    expect(error).toEqual(new InvalidParamError(field))
+  })
+})
