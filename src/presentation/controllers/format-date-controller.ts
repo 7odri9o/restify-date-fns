26d10754d9d751dc78dc50defecfadca90eb3917
Rodrigo Rocha <act111@einstein.br>
2022-03-19 03:51:10 -0300

    refactor: remove unecessary tests from presentation layer

--- src/presentation/controllers/format-date-controller.ts
+++ src/presentation/controllers/format-date-controller.ts
@@ -4,14 +4,11 @@ export class FormatDateController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
-    const requiredFields = ['date', 'format']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return {
-          status: 400,
-          body: new Error(`Missing param: ${field}`)
-        }
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return {
+        status: 400,
+        body: error
       }
     }
   }
--- tests/presentation/controllers/format-date-controller.spec.ts
+++ tests/presentation/controllers/format-date-controller.spec.ts
@@ -17,40 +17,6 @@ type SutTypes = {
 }
 
 describe('FormatDate Controller', () => {
-  test('Should return 400 if format is not provided', async () => {
-    const { sut } = makeSut()
-
-    const httpRequest = {
-      body: {
-        date: 'any_date'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-
-    const expected = {
-      status: 400,
-      body: new Error('Missing param: format')
-    }
-    expect(httpResponse).toEqual(expected)
-  })
-
-  test('Should return 400 if date is not provided', async () => {
-    const { sut } = makeSut()
-
-    const httpRequest = {
-      body: {
-        format: 'any_format'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-
-    const expected = {
-      status: 400,
-      body: new Error('Missing param: date')
-    }
-    expect(httpResponse).toEqual(expected)
-  })
-
   test('Should call Validation with correct values', async () => {
     const { sut, validationSpy } = makeSut()
 
